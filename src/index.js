97dd2c8339792a3a6d88299b59cdb0187de4c2eb
Shivanshu <craziest.oliyhan13@gmail.com>
2024-06-06 01:02:06 +0530

    adding utility functions

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
+
 connectDB()
+.then(() => {
+
+    app.on("error", (error)=> {
+        console.log(`Error connecting with app: ${error}`);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(` Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log(`DB Connection failed: ${error}`);
+})
 
 
 
